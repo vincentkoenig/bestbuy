8bc624f34639d34b457e206adf1cce6aa9b03848
Vincent König <vincentkoenig97@gmail.com>
2026-04-21 15:39:59 +0200

    Produktklasse erstellt

---
+++ products.py
@@ -0,0 +1,64 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if name == "":
+            raise Exception("Name cannot be empty")
+        self.name = name
+
+        if price < 0:
+            raise Exception("Price cannot be negative")
+        self.price = price
+
+        if quantity < 0:
+            raise Exception("Quantity cannot be negative")
+        self.quantity = quantity
+
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+        if quantity == 0:
+            self.deactivate()
+        else:
+            self.activate()
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        if quantity <= 0:
+            raise Exception("Quantity must be positive")
+        if quantity > self.quantity:
+            raise Exception("Not enough quantity in stock")
+
+        total_price = quantity * self.price
+        self.set_quantity(self.quantity - quantity)
+        return total_price
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    bose.show()
+    mac.show()
+
+    bose.set_quantity(1000)
+    bose.show()
+
+if __name__ == "__main__":
+    main()
